a3e01f4d0bf5fb4dfb435414d1579f887cc078a6
Elian-Tester <eliancruz01@gmail.com>
2022-03-03 16:19:15 -0600

    Add regex para expresion regular

--- src/sample/model/Automata.java
+++ src/sample/model/Automata.java
@@ -3,119 +3,100 @@ package sample.model;
 import javafx.scene.control.Alert;
 import sample.controller.Controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Automata {
-    char [] alfabeto =  {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    char [] arregloNumeros = {'0','1','2','3','4','5','6','7','8','9'};
+
     Controller ventana = new Controller();
 
     //EVALUAR PRIMER CARACTER
-   public void q0(String rfcConstruido, String rfcIngresado)
-   {
-       int contador=0;
-       for (int i = 0; i < 4; i++) {
-           if (recorrer(rfcIngresado.charAt(i))){
-               System.out.println("correcto");
-               contador++;
-           }
-       }
-       if (contador==4)
-       {
-           q1(rfcConstruido,rfcIngresado);
-       }
-       else
-           ventana.mostrarIvalidacion(rfcIngresado.charAt(contador));
-   }
+    public void q0(String rfcConstruido, String rfcIngresado) {
 
+        Pattern patron = Pattern.compile("^[A-Z]");
 
-   public void q1(String rfcConstruido, String rfcIngresado)
-   {
-       //HEAK981125
-       int contador=4;
-       for (int i = 4; i < 10; i++) {
-           if (recorrerNumeros(rfcIngresado.charAt(i))){
-               System.out.println("numero correcto");
-               contador++;
-           }
-       }
-       if (contador==10)
-       {
-           System.out.println("todos son numeros");
-           q2(rfcConstruido,rfcIngresado);
-       }
-       else
-           ventana.mostrarIvalidacion(rfcIngresado.charAt(contador));
-   }
-
-   public void q2(String rfcConstruido, String rfcIngresado)
-   {
-       String anio = String.valueOf(rfcIngresado.charAt(4))+ String.valueOf(rfcIngresado.charAt(5));
-       int digito = Integer.parseInt(anio);
-       if ((digito>=50 && digito<=99) ||(digito>=0 && digito<=22) ){
-           System.out.println("año correctoBRYAN");
-           String mes = String.valueOf(rfcIngresado.charAt(6))+ String.valueOf(rfcIngresado.charAt(7));
-           int digitomes = Integer.parseInt(mes);
-           if ((digitomes>0 && digitomes<=12))
-           {
-               System.out.println("mes correcto");
-               String dia = String.valueOf(rfcIngresado.charAt(8))+ String.valueOf(rfcIngresado.charAt(9));
-               int digitodia = Integer.parseInt(dia);
-               if ((digitodia>0 && digitodia<=31))
-               {
-                   System.out.println("fecha correcta");
-                   q3(rfcConstruido,rfcIngresado);
-               }else
-               {
-                   String m = "El dia ["+dia+"] no es válido";
-                   ventana.mostrarIvalidacionFecha(m);
-               }
-           }
-           else {
-               String m = "El mes ["+mes+"] no es válido";
-               ventana.mostrarIvalidacionFecha(m);
-           }
-       }
-       else
-       {
-           String m = "El año ["+anio+"] no es válido";
-           ventana.mostrarIvalidacionFecha(m);
-       }
-       System.out.println(anio + digito);
-   }
-
-
-   public void q3(String rfcConstruido, String rfcIngresado)
-   {
-       if (rfcConstruido.equals(rfcIngresado))
-       {
-           String m = " FELICIDADES ONICHAN HAS APRENDIDO A GENERAR TU RFC <3";
-           ventana.mostrarCorrecto(m, Alert.AlertType.INFORMATION);
-       }
-       else
-       {
-           String m = "EL RFC QUE INGRESASTE NO ES EL TUYO :(";
-           ventana.mostrarCorrecto(m, Alert.AlertType.ERROR);
-       }
-   }
-
-    public boolean recorrer(char letra)
-    {
-        for (int i = 0; i <alfabeto.length; i++) {
-            if (letra==alfabeto[i])
-            {
-                return true;
+        int contador = 0;
+        for (int i = 0; i < 4; i++) {
+            if (patron.matcher(String.valueOf(rfcIngresado.charAt(i))).find()) {
+                System.out.println("correcto compile");
+                contador++;
+            } else {
+                break;
             }
         }
-        return false;
+        if (contador == 4) {
+            q1(rfcConstruido, rfcIngresado);
+        } else {
+            ventana.mostrarIvalidacion(rfcIngresado.charAt(contador));
+        }
     }
 
-    public boolean recorrerNumeros(char letra)
-    {
-        for (int i = 0; i <arregloNumeros.length; i++) {
-            if (letra==arregloNumeros[i])
-            {
-                return true;
+
+    public void q1(String rfcConstruido, String rfcIngresado) {
+        //HEAK981125
+        Pattern patron = Pattern.compile("^[0-9]");
+
+        int contador = 4;
+        for (int i = 4; i < 10; i++) {
+            if (patron.matcher(String.valueOf(rfcIngresado.charAt(i))).find()) {
+                System.out.println("numero correcto compile");
+                contador++;
+            } else {
+                break;
             }
         }
-        return false;
+        if (contador == 10) {
+            q2(rfcConstruido, rfcIngresado);
+        } else
+            ventana.mostrarIvalidacion(rfcIngresado.charAt(contador));
     }
+
+    public void q2(String rfcConstruido, String rfcIngresado) {
+        String anio = String.valueOf(rfcIngresado.charAt(4)) + String.valueOf(rfcIngresado.charAt(5));
+
+        Pattern patronAge1 = Pattern.compile("[5-9][0-9]");
+        Pattern patronAge2 = Pattern.compile("[0-1][0-9]");
+        Pattern patronAge3 = Pattern.compile("2[0-2]");
+
+        if (patronAge1.matcher(String.valueOf(anio)).find() || patronAge2.matcher(String.valueOf(anio)).find() || patronAge3.matcher(String.valueOf(anio)).find()) {
+            System.out.println("año correcto compiler: " + anio);
+            String mes = String.valueOf(rfcIngresado.charAt(6)) + String.valueOf(rfcIngresado.charAt(7));
+
+            Pattern patronMes1 = Pattern.compile("0[1-9]");
+            Pattern patronMes2 = Pattern.compile("1[0-2]");
+            if (patronMes1.matcher(mes).find() || patronMes2.matcher(mes).find()) {
+                System.out.println("mes correcto compiler");
+                String dia = String.valueOf(rfcIngresado.charAt(8)) + String.valueOf(rfcIngresado.charAt(9));
+
+                Pattern patronDia1 = Pattern.compile("0[1-9]");
+                Pattern patronDia2 = Pattern.compile("1|2[0-9]");
+                Pattern patronDia3 = Pattern.compile("3[0-1]");
+                if (patronDia1.matcher(dia).find() || patronDia2.matcher(dia).find() || patronDia3.matcher(dia).find()) {
+                    System.out.println("fecha correcta compiler");
+                    q3(rfcConstruido, rfcIngresado);
+                } else {
+                    String m = "El dia [" + dia + "] no es válido";
+                    ventana.mostrarIvalidacionFecha(m);
+                }
+            } else {
+                String m = "El mes [" + mes + "] no es válido";
+                ventana.mostrarIvalidacionFecha(m);
+            }
+        } else {
+            String m = "El año [" + anio + "] no es válido";
+            ventana.mostrarIvalidacionFecha(m);
+        }
+    }
+
+
+    public void q3(String rfcConstruido, String rfcIngresado) {
+        if (rfcConstruido.equals(rfcIngresado)) {
+            String m = " FELICIDADES ONICHAN HAS APRENDIDO A GENERAR TU RFC <3";
+            ventana.mostrarCorrecto(m, Alert.AlertType.INFORMATION);
+        } else {
+            String m = "EL RFC QUE INGRESASTE NO ES EL TUYO :(";
+            ventana.mostrarCorrecto(m, Alert.AlertType.ERROR);
+        }
+    }
+
 }
